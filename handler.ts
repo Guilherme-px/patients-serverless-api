fa23b7a96ca72f17e4058e65eeeb930368e6a7e8
Guilherme <guilherme.s.goncalves@outlook.com>
2022-12-29 18:56:11 -0300

    add cors

--- handler.ts
+++ handler.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { Response, Request, NextFunction } from 'express';
 import { CelebrateError } from 'celebrate';
 import serverless from 'serverless-http';
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use('/patients', patientRoutes);
 
